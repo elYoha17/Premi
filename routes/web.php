cad430d66e42626999a4f417c7f957c3d17cd282
Yohanan Miteo <yohananmiteo17@gmail.com>
2023-08-19 06:32:58 +0100

    feat: define products index page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', HomeController::class)->name('home');
+
+    Route::resource('products', ProductController::class)->only('index');
 });
 
 Route::middleware('auth')->group(function () {
