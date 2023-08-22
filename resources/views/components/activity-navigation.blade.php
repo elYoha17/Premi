e6c7c2d246578174a2eadce53d1bb02c807c1b38
Yohanan Miteo <yohananmiteo17@gmail.com>
2023-08-22 11:54:22 +0100

    fix: add link activation

--- resources/views/components/activity-navigation.blade.php
+++ resources/views/components/activity-navigation.blade.php
@@ -4,7 +4,7 @@
 
 <div class="bg-slate-700">
     <div class="flex justify-center items-center max-w-7xl mx-auto py-1.5 space-x-2">
-        <a href="{{ route('activities.show', [$activity]) }}" class="px-2 py-1 text-white text-sm tracking-wide rounded hover:bg-white/25 {{ request()->routeIs('products.show') ? 'text-white' : 'text-gray-400' }}">Tableau de bord</a>
-        <a href="{{ route('activities.sales.index', [$activity]) }}" class="px-2 py-1 text-white text-sm tracking-wide rounded hover:bg-white/25 {{ request()->routeIs('products.show') ? 'text-white' : 'text-gray-400' }}">Vente</a>
+        <a href="{{ route('activities.show', [$activity]) }}" class="px-2 py-1 text-sm tracking-wide rounded hover:bg-white/25 {{ request()->routeIs('activities.show') ? 'text-white' : 'text-gray-400 hover:text-white' }}">Tableau de bord</a>
+        <a href="{{ route('activities.sales.index', [$activity]) }}" class="px-2 py-1 text-sm tracking-wide rounded hover:bg-white/25 {{ request()->routeIs('activities.sales.index') ? 'text-white' : 'text-gray-400 hover:text-white' }}">Vente</a>
     </div>
 </div>
